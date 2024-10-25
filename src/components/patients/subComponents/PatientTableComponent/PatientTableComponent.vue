70bbfe0a9b6ba0d3d9eddaaa6da7a07bd06f81d8
camilosalgadocastro <casc1902@hotmail.com>
2024-10-25 07:35:44 -0500

    Listing Patient on table 25-10-2024 1

--- src/components/patients/subComponents/PatientTableComponent/PatientTableComponent.vue
+++ src/components/patients/subComponents/PatientTableComponent/PatientTableComponent.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 
 import {CCard, CCardBody} from "@coreui/vue/dist/esm/components/card";
-import {reactive} from "vue";
+import {onMounted, reactive} from "vue";
+import PatientService from "@/services/patients/Patient.service.ts";
+import {CButton} from "@coreui/vue/dist/esm/components/button";
 
 // Init Your table settings
 const table = reactive({
@@ -10,22 +12,42 @@ const table = reactive({
     {
       label: "ID",
       field: "id",
-      width: "3%",
+      width: '3%',
       sortable: true,
       isKey: true,
     },
     {
-      label: "Name",
-      field: "name",
-      width: "10%",
+      label: "T. Doc",
+      field: "tipo_doc",
       sortable: true,
+      isKey: true,
+    },
+    {
+      label: "Paciente",
+      field: "numero_documento",
+      sortable: true,
+      isKey: true,
+    },
+    {
+      label: "G.S. RH",
+      field: "grupo_sanguineo",
+      sortable: true,
+      isKey: true,
     },
     {
       label: "Email",
       field: "email",
-      width: "15%",
       sortable: true,
     },
+    {
+      label: "Teléfono",
+      field: "telefono_celular",
+      sortable: true,
+    },
+    {
+      label: "Acciones",
+      field: "actions",
+    }
   ],
   rows: [],
   totalRecordCount: 0,
@@ -35,23 +57,57 @@ const table = reactive({
   },
 })
 
+const getPatientList = async () => {
+
+  table.isLoading = true
+
+  try {
+
+    const {data} = await PatientService.getAllPatients()
+    table.rows = data.data
+
+    table.isLoading = false
+
+  } catch (error) {
+    table.isLoading = false
+    console.log(error.response.message)
+  }
+
+}
+
+const removeItemFromTable = (id: number) => {
+  table.rows = table.rows.filter(item => item.id !== id)
+}
+
+onMounted(() => {
+  getPatientList()
+})
+
 </script>
 
 <template>
 
-  <CCard>
-    <CCardBody>
-      <table-lite
-          :is-loading="table.isLoading"
-          :columns="table.columns"
-          :rows="table.rows"
-          :total="table.totalRecordCount"
-          :sortable="table.sortable"
-      >
+  <div>
+    <table-lite
+        :is-slot-mode="true"
+        :is-loading="table.isLoading"
+        :columns="table.columns"
+        :rows="table.rows"
+        :total="table.totalRecordCount"
+        :sortable="table.sortable"
+        @is-finished="table.isLoading = false"
+    >
 
-      </table-lite>
-    </CCardBody>
-  </CCard>
+      <template v-slot:actions="data">
+
+        <CButton @click.prevent="removeItemFromTable(data.value.id)" :title="`Eliminar Paciente ${data.value.numero_documento}`">
+          <CIcon icon="cil-trash" />
+        </CButton>
+
+      </template>
+
+    </table-lite>
+  </div>
 
 </template>
 
